91e501a5543fc287ca8b7793cecb771146d8c3d8
Eastern  1306 <dong.nguyen@cyberlogitec.com>
2025-02-14 16:14:40 +0700

    update docker-file auth node version

--- backend/libs/database/src/lib/typeorm.config.ts
+++ backend/libs/database/src/lib/typeorm.config.ts
@@ -3,8 +3,8 @@ import {User} from "../../../../apps/entities/user.entity"
 export const AppDataSource = () => {
     return new DataSource({
         type: 'postgres',
-        host: 'localhost',
-        port: 5433,
+        host: 'db',
+        port: 5432,
         username: process.env["POSTGRES_USERNAME"],
         password: process.env["POSTGRES_PASSWORD"],
         database: process.env["POSTGRES_DB"],
